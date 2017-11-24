18299707a4c7c0470f34ee61adf429838ea3dbf2
Yilin He <yilin.he@outlook.com>
2017-11-24 18:10:05 +0800

    Add files via upload

--- image_fft2.m
+++ image_fft2.m
@@ -1,6 +1,12 @@
-%This fuction is used to make a FFT or IFFT to a image.
+%This function is used to make a FFT or IFFT to a gray image.
 function im_FFT=image_fft2(Name_image)
-S_image = imread(Name_image);
+
+if class(Name_image==char)
+    S_image = imread(Name_image);
+else
+    S_image = Name_image;
+end
+
 if length(size(S_image))==3
     image = mat2gray(rgb2gray(S_image));
 else
