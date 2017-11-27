af3629952d1ff6b8136364fe0699fb962746e9f0
Yilin He <yilin.he@outlook.com>
2017-11-27 18:24:57 +0800

    Add files via upload

--- image_fft2.m
+++ image_fft2.m
@@ -1,7 +1,7 @@
 %This function is used to make a FFT or IFFT to a gray image.
 %input can be a char or a mat
 %output is a mat
-function im_FFT=image_fft2(Name_image)
+function im_FFT = image_fft2(Name_image)
 
 if ischar(Name_image)
     S_image = imread(Name_image);
@@ -9,36 +9,37 @@ else
     S_image = Name_image;
 end
 
-if length(size(S_image))==3
+if length(size(S_image)) == 3
     image = mat2gray(rgb2gray(S_image));
 else
     image = mat2gray(S_image);
 end
 
-subplot(221);imshow(image);
+figure;
+subplot(221); imshow(image);
 title('Source');
 
 im_fft_d = fft2(image);
-im_fft = log(1+abs(im_fft_d));
-subplot(222);imshow(im_fft);
+im_fft = abs(im_fft_d);
+subplot(222); imshow(im_fft);
 title('FFT');
 
 %im_FFT is the image through fftshift conversion
 im_FFT_d = fftshift(im_fft_d);
-im_FFT = log(1+abs(im_FFT_d));
-subplot(223);imshow(im_FFT);
+im_FFT = abs(im_FFT_d);
+subplot(223); imshow(im_FFT);
 title('FFTshift');
 
-im_ifft_d=ifft2(ifftshift(im_FFT_d));
-im_ifft = log(1+abs(im_ifft_d));
-subplot(224);imshow(im_ifft);
+im_ifft_d = ifft2(ifftshift(im_FFT_d));
+im_ifft = abs(im_ifft_d);
+subplot(224); imshow(im_ifft);
 title('IFFT');
 
-figure(2);
-surf(im_FFT);shading interp;
+figure;
+surf(im_FFT); shading interp;
 title('3D FFT');
 
-figure(3);
+figure;
 contour3(im_FFT);
 title('Contour line')
 end
--- makeImage.m
+++ makeImage.m
@@ -1,41 +1,62 @@
 clear all;clc;close all;
 
 % Generate an ellipsoid
-% a=200, b=200, c=20
-hight = zeros(512, 512);
+% a=200, b=200, c=30
+hight = zeros(513, 513);
+a_el = 200; b_el = 200; c_el = 30;
+a2_el = a_el^2; b2_el = b_el^2; c2_el = c_el^2;
 for ii = 1 : 512
     for jj = 1 : 512
-        if (((ii-256)^2+(jj-256)^2) < 4e4)
-            hight(ii,jj) = sqrt(4e2-((ii-256)^2+(jj-256)^2)/1e2);
+        if (((ii-257)^2*b2_el+(jj-257)^2*a2_el) <= a2_el*b2_el)
+            hight(ii,jj) = sqrt((a2_el*b2_el*c2_el-...
+                b2_el*c2_el*(ii-257)^2-a2_el*c2_el*(jj-257)^2)...
+                /(a2_el*b2_el));
         end
     end
 end
-figure; surf(hight);
+figure; surf(hight, 'CDataMapping', 'scaled'); caxis auto; shading interp;
 
 % Generate a sine grating
-% T = 4, f = 1/4
-T_grating = 4; f_grating = 1/T_grating;
+% T = 80, f = 1/80
+T_grating = 80; f_grating = 1/T_grating;
 
 I_grating = zeros(size(hight));
 for jj = 1 : 512
-     I_grating(:, jj) = 128+127*cos(2*pi*jj*f_grating);
+     I_grating(:, jj) = 128+127*cos(2*pi*jj*f_grating);   
 end
-figure; 
-imshow(mat2gray(I_grating));
+figure; image_grating = mat2gray(I_grating);
+imshow(image_grating); title('image-grating');
+imwrite(image_grating, 'test1.bmp');
 
 % theta is the angle of incidence
 % lambda is the equivalent length
 theta = pi/4;
 lambda_eq = T_grating*sin(theta);
 
+% E_angle_add is the additional phase's angle of image from object
 E_image = zeros(size(hight));
-for ii = 1 : 512
-    for jj = 1 : 512
-     E_image(ii,jj) = 128+127*cos(2*pi*jj*f_grating+2*pi*hight(ii,jj)/lambda_eq);
+E_phase = zeros(size(hight));
+E_angle_add = zeros(size(hight));
+for ii = 1 : 513
+    for jj = 1 : 513
+        E_angle_add(ii,jj) = hight(ii,jj)/lambda_eq;
+        E_phase(ii,jj) = 2*pi*jj*f_grating+2*pi*E_angle_add(ii,jj);
+        E_image(ii,jj) = 0.5+0.5*cos(E_phase(ii,jj));
     end  
 end
 
 figure; 
 E_image = mat2gray(E_image);
-imshow(E_image);
-imwrite(E_image, 'test.bmp');
+imshow(E_image); title('E-image');
+imwrite(E_image, 'test2.bmp');
+
+figure; 
+E_angle_phase = mod(E_phase/2/pi,1);
+image(E_angle_phase, 'CDataMapping', 'scaled'); 
+colormap(jet); title('E-angle-phase'); colorbar;
+imwrite(E_angle_phase, 'test3.bmp');
+
+figure;
+image(E_angle_add, 'CDataMapping', 'scaled'); caxis([0 1]) 
+colormap(jet); title('E-angle-add'); colorbar;
+imwrite(E_angle_add, 'test3.bmp');
