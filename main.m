3dc747d0a1bfbaa537c06f1332eee552d07d2d77
Yilin He <yilin.he@outlook.com>
2017-12-25 22:05:07 +0800

    Add files via upload

--- main.m
+++ main.m
@@ -1,10 +1,16 @@
 clear; close all;
 
-%%
+%% Adding all subfolder to the path
+addpath(genpath(pwd));
+Path = pwd;
+
+%% Generate theoretical images
 [ img_s,  phase_grating, phase_add_i ] = makeImage;
 
-%%
-img1 = image_fft2( 'test2.bmp' );
+%% FFT the input image
+img1 = image_fft2( [Path '\Image\InputImage.bmp'] );
+
+%% Filter out the highest peak of input image
 [ a1, b1, x1, y1 ] = find_range( img1 );
 img2 = cut_ellipse_hft( a1, b1, x1 ,y1, img1);
 
@@ -12,27 +18,27 @@ figure;
 surf( abs( img2 ) ); shading interp;
 title('3D img after High-pass filter ');
 
-%%
+%% Filter out the objective peak of input image
 [ a2, b2, x2, y2 ] = find_range( img2 );
 img3 = cut_ellipse_lft( a2, b2, x2 ,y2, img2);
-
-%img3 = zeros( size( img2 ) ); img3( :, x1 : end ) = img2( :, x1 : end );
 
 figure;
 surf( abs( img3 ) ); shading interp;
 title('3D img after Low-pass filter ');
 
-%%
-agl_ifft = image_ifft2( img3 );
-phase_out = agl_ifft/2/pi+0.5;
-phase_add_o = abs( phase_out - phase_grating ) ;
+%% IFFT the output image
+img_out = image_ifft2( img3 );
 
-figure;
-image(phase_out, 'CDataMapping', 'scaled'); caxis([0 1]) 
-colormap(jet); title('Output-phace'); colorbar;
+%% Unwrapped the phase of image
+phase_unwrapped = QualityGuidedUnwrap2D( img_out );
+
+%%
+phase_out = phase_unwrapped - phase_grating;
+
+save([ Path '\Mat\Im.mat']);
 
 %{
 figure;
-image(phase_add_o, 'CDataMapping', 'scaled'); caxis([0 1]) 
-colormap(jet); title('Output-phace-add'); colorbar;
+image(phase_out, 'CDataMapping', 'scaled'); caxis([0 1]) 
+colormap(jet); title('Output-phace'); colorbar;
 %}
