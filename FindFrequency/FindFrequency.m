a0ff193f11f684fddca78a52b97625cc91aa8d4c
Yilin He <yilin.he@outlook.com>
2018-02-02 12:21:18 +0800

    update

--- FindFrequency/FindFrequency.m
+++ FindFrequency/FindFrequency.m
@@ -1,5 +1,10 @@
 %% This function is used to find the frequency of the grating
 function fs = FindFrequency( img )
-img_fft = abs( fftshift( fft2( img ) ) );
+[ row, columns ] = sizes( img );
+NFFT_row = 2^nextpow2( row );
+NFFT_columns = 2^nextpow2( columns );
+img_fft = abs( fftshift( fft2(img,[ NFFT_row,NFFT_columns ]) ) );
 N = numel( img_fft );
-sort()
+[ ~, index ] = sort( img_fft(:) );
+n = floor( 0.05*N );
+img_fft(index)
