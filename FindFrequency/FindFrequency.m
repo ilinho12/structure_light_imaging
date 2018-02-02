de6c4f97e3236b7b7cc6762da56b60d5826d552b
Yilin He <yilin.he@outlook.com>
2018-02-02 23:34:08 +0800

    Add files via upload

--- FindFrequency/FindFrequency.m
+++ FindFrequency/FindFrequency.m
@@ -3,8 +3,16 @@ function fs = FindFrequency( img )
 [ row, columns ] = sizes( img );
 NFFT_row = 2^nextpow2( row );
 NFFT_columns = 2^nextpow2( columns );
-img_fft = abs( fftshift( fft2(img,[ NFFT_row,NFFT_columns ]) ) );
-N = numel( img_fft );
-[ ~, index ] = sort( img_fft(:) );
+img_fft1 = abs( fft2(img,[ NFFT_row,NFFT_columns ]) )/( row*columns );
+img_fft2 = img_fft1( 1:row/2+1, 1:columns+1 );
+N = numel( img_fft2 );
+[ ~, index ] = sort( img_fft2(:) );
 n = floor( 0.05*N );
-img_fft(index)
+img_fft2( 1:n ) = -1;
+f_row = Fs*( 0:( row/2 ) )/row;
+f_columns = Fs*( 0:( columns/2 ) )/columns;
+[ ind_row, ind_colums ] = find( img_fft2(:) == -1 );
+fs_row = mean( f_row( ind_row ) );
+fs_coluns = mean( f_columns( ind_colums ) );
+fs = sqrt( fs_coluns+fs_row );
+end
